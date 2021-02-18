61ed9eac5258cf9bc88a812b8d249ce66d3cf35d
mimbol <mim.bol@mail.ru>
2021-02-18 22:35:50 +0500

    404? Не, не слышал

--- init.php
+++ init.php
@@ -6,9 +6,17 @@ function deb($k,$v){
 function json(string $src){
     return json_decode(file_get_contents($src),1);
 }
+function locat($site,$time = 0){
+    echo "<meta http-equiv='refresh' content='$time; url=$site'>";
+}
+function e404(){
+    echo "<meta http-equiv='refresh' content='0; url=/404'>";
+    die();
+}
+deb("get",$_GET);
 $config = json("config.json");
 if($config['system']['offline']){
-    include 'offline.php';
+    include 'pages/offline.php';
     die();
 }
 deb("conf",$config);
--- main.php
+++ main.php
@@ -1,3 +1,8 @@
 <?php
 $routers = json("routers.json");
 deb('routs',$routers);
+if($routers[$_GET['page']] == ""){
+	e404();
+}
+include 'controllers/'.$routers[$_GET['page']];
+echo controller("");
