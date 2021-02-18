25a3f5e12cf7d4994cad84d07fad6fefcb7c7ad7
mimbol <mim.bol@mail.ru>
2021-02-18 22:58:05 +0500

    чайлдфри

--- main.php
+++ main.php
@@ -4,5 +4,5 @@ deb('routs',$routers);
 if($routers[$_GET['page']] == ""){
 	e404();
 }
-include 'controllers/'.$routers[$_GET['page']];
-echo controller("");
+include 'controllers/'.$routers[$_GET['page']].".php";
+echo controller(file_get_contents('templates/'.$routers[$_GET['page']].".nyan"));
