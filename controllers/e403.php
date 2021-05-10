392aa2b9c648beedcbc62f3b215d9629bc369923
Александр Екемеров <mim.bol@mail.ru>
2021-05-10 23:03:51 +0300

    Горшочек, вари!

---
+++ controllers/e403.php
@@ -0,0 +1,5 @@
+<?php
+function e403(Object $obj, array $params = [])
+{
+    return $obj->template;
+}
---
+++ controllers/e404.php
@@ -0,0 +1,5 @@
+<?php
+function e404(Object $obj, array $params = [])
+{
+    return $obj->template;
+}
